520233c17fac1819443521bed91bc0e8b488bed0
Devang <devang.radical@gmail.com>
2020-07-12 17:44:56 +0530

    second

--- link_short/templates/index.php
+++ link_short/templates/index.php
@@ -55,10 +55,11 @@ h5{
 </head>  
 <body>
 <div id="particles-js"></div>
+   
     <div class="container-fluid h-100 px-3 py-3">
       <div class="row h-100 justify-content-center align-items-center">
- <form method="post" id="myform" class=" col-10 px-2 py-2">
-<div class="text col-12 text-center mb-5">Short Url</div>
+      	<form method="post" id="myform" class=" col-10 px-2 py-2">
+      		<div class="text col-12 text-center mb-5">Short Url</div>
  {% csrf_token %}
   
   <div class="input-group form-group mb-0">
@@ -73,9 +74,9 @@ h5{
   <div class="text-light post">
   </div>
 </div>
-</div>
 
 </form>
+</div>
 
 </div>
 <script>
